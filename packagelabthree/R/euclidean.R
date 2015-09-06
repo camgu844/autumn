2b8fe4f71d15bb697f9b90bc05e784da718eb304
Camilla Skoglund <camgu844@student.liu.se>
2015-09-06 21:19:34 +0200

    created package packagelabthree

---
+++ packagelabthree/R/euclidean.R
@@ -0,0 +1,22 @@
+#' Finds the greatest common divisor of two numbers.
+#' 
+#' The euclidian algorithm finds the greatest common divisor of two numbers. 
+#' More about the algorithm can be found at https://en.wikipedia.org/wiki/Euclidean_algorithm.
+#' 
+#' @param a A numeric scalar or integer.
+#' @param b A numeric scalar or integer.
+#' @return A numeric value which is the greatest common divisor of a and b.
+#' 
+
+# 1.1.1 euclidean()
+euclidean <- function (a,b) {
+  stopifnot(is.numeric(a) && length(a)==1)
+  stopifnot(is.numeric(b) && length(b)==1)
+  while (!b==0) {
+    t <- b
+    b <- a %% b
+    a <- t
+  }
+  return(a)
+}
+
