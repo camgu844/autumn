e787cb33c366c06949c9a5a38f4499dd5a7627ec
Camilla Skoglund <camgu844@student.liu.se>
2015-09-09 08:49:13 +0200

    changes in test files

--- packagelabthree/tests/testthat/test_correct_value_out.R
+++
@@ -1,8 +0,0 @@
-library(packagelabthree)
-context("output is correct")
-
-test_that("the output value is correct", {
-  expect_that(euclidean(100,100), 100)
-  expect_that(euclidean(5,1), 1)
-  expect_that(euclidean(40,8), 8)
-})
---
+++ packagelabthree/tests/testthat/test_numeric_arguments_and_value_out.R
@@ -0,0 +1,16 @@
+library(packagelabthree)
+context("arguments are numeric and output is correct")
+
+eu <- euclidean(100,100)
+
+test_that("the input arguments are numeric, of length 1 and whole numbers", {
+  expect_that(eu, is_a("numeric"))
+  expect_that(length(eu), equals(1))
+  expect_that(euclidean(2.5,10), throws_error())
+})
+
+test_that("the output value is correct", {
+  expect_that(euclidean(100,100), 100)
+  expect_that(euclidean(5,1), 1)
+  expect_that(euclidean(40,8), 8)
+})
